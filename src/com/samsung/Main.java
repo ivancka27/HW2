5e3a35dff3ba1a5265fbc81d6d4594c7c0d9fa85
ASUS <ivancka27@mail.ru>
2024-10-14 16:05:23 +0300

    Try

--- src/com/samsung/Main.java
+++ src/com/samsung/Main.java
@@ -1,18 +1,28 @@
 package com.samsung;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        if (a / 10 == 0 && a >= 0) {
-            System.out.println("DIGIT");
-        } else if (a / 100 == 0 && a / 10 != 0 && a > 0) {
-            System.out.println("NUM");
-        } else {
-            System.out.println("OTHER");
+        in.useLocale(Locale.US);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = -b / a;
+        double e = -c;
+        String d = new DecimalFormat("#0.0").format(c);
+        String f = new DecimalFormat("#0.0").format(e);
+        if (a == 0 && b <= 0 || a < 0 && c < 0) {
+            System.out.println("no such x");
+        } else if (a > 0 && c < 0 || a == 0 && b > 0) {
+            System.out.println("any x");
+        } else if (a < 0 && c > 0) {
+            System.out.println(f + "<x<" + d);
+        }else if (a > 0 && c > 0) {
+            System.out.println("x<" + f + " or x>" + d);
         }
     }
 }
